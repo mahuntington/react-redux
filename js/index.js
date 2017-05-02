becb8d0134b21dde8a81e36748ff44aceb6bacec
Matt Huntington <matt.huntington@gmail.com>
2017-05-02 14:29:11 -0400

    created component

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import Heading from './components/heading.js';
 
 ReactDOM.render(
-  <h1 className="foo">Hello, world!</h1>,
+  <Heading title="Matt"/>,
   document.querySelector('main')
 );
