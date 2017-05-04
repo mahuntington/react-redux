1cc585ba9973d5c4ae9277381c0f80030ddef027
Matt Huntington <matt.huntington@gmail.com>
2017-05-03 22:03:10 -0400

    fetch examples

--- js/index.js
+++ js/index.js
@@ -4,11 +4,27 @@ import App from './components/app.js';
 import { Provider } from 'react-redux'
 import store from './store.js'
 
-fetch('https://stupidcomments.herokuapp.com/comments').then(function(response){
-    response.json().then(function(data){
-        console.log(data);
-    });
-});
+// fetch('https://stupidcomments.herokuapp.com/comments').then(function(response){
+//     response.json().then(function(data){
+//         console.log(data);
+//     });
+// });
+
+// fetch(
+//     'https://stupidcomments.herokuapp.com/comments',
+//     {
+//         headers: {
+//             'Accept': 'application/json',
+//             'Content-Type': 'application/json'
+//         },
+//         method: "POST",
+//         body: JSON.stringify({ body:'from react' })
+//     }
+// ).then(function(response){
+//     response.json().then(function(data){
+//         console.log(data);
+//     });
+// });
 
 ReactDOM.render(
     <Provider store={store}>
