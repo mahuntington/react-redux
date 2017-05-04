fbe1616d26d875723597c265cb6904e4e8eda81c
Matt Huntington <matt.huntington@gmail.com>
2017-05-03 21:49:15 -0400

    ajax with fetch

--- js/index.js
+++ js/index.js
@@ -4,6 +4,12 @@ import App from './components/app.js';
 import { Provider } from 'react-redux'
 import store from './store.js'
 
+fetch('https://stupidcomments.herokuapp.com/comments').then(function(response){
+    response.json().then(function(data){
+        console.log(data);
+    });
+});
+
 ReactDOM.render(
     <Provider store={store}>
         <App/>
